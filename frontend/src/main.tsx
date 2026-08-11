7e8ff694324c35d1e986045e50fd7595b1e2c561
msk50 <igarashi.misaki.624@s.kyushu-u.ac.jp>
2026-08-11 19:15:51 +0900

    feat: パスワード変更画面とメールアドレス変更画面を作成

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 import App from './App.tsx'
 import Signup from './auth/Signup.tsx'
 import Signin from './auth/Signin.tsx'
+import ChangePassword from './auth/ChangePassword.tsx'
+import ChangeEmail from './auth/ChangeEmail.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +15,8 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/change-password" element={<ChangePassword/>} />
+        <Route path="/change-email" element={<ChangeEmail/>} />      
       </Routes>
     </BrowserRouter>
   </StrictMode>,
